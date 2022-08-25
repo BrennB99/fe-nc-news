b8104223970e1f2c5a346d957c220fb67fc526f2
BrennB99 <ben.brennan249@gmail.com>
2022-08-25 12:04:35 +0100

    able to post comments on articles

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,10 +1,16 @@
-import { useEffect, useState } from "react";
-import { fetchComments } from "../api";
+import { useEffect, useState, useContext } from "react";
+import { fetchComments, postComment } from "../api";
 import { useParams } from "react-router-dom";
+import { UserContext } from "../contexts/User";
 
 export default function Comments() {
+  const { user } = useContext(UserContext);
   const [comments, setComments] = useState("");
   const [isLoading, setIsLoading] = useState(true);
+  const [newComment, setNewComment] = useState({
+    username: user,
+    body: undefined,
+  });
   let { article_id } = useParams();
 
   useEffect(() => {
@@ -15,13 +21,48 @@ export default function Comments() {
     });
   }, [article_id]);
 
+  const handleChange = (event) => {
+    const comment = { ...newComment };
+    comment[event.target.id] = event.target.value;
+    setNewComment(comment);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(newComment);
+    postComment(article_id, newComment).then(() => {
+      fetchComments(article_id).then(({ comments }) => {
+        setComments(comments);
+      });
+    });
+  };
+
   if (isLoading) {
     return <p>Loading...</p>;
   }
-  console.log(comments);
+
   return (
     <>
       <h5>Comments</h5>
+
+      <form
+        onSubmit={(event) => {
+          handleSubmit(event);
+        }}
+      >
+        <label htmlFor="body">
+          Share Your Thoughts:
+          <input
+            type="text"
+            id="body"
+            value={newComment.body}
+            onChange={(event) => {
+              handleChange(event);
+            }}
+          />
+        </label>
+        <input type="submit" value="Submit!"></input>
+      </form>
 
       {comments.map((comment) => {
         return (
